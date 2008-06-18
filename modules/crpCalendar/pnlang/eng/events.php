365357fdd43a1467cffc0c0b10a26a35a9de6d4e
jamiroconca <conca.daniele@gmail.com>
2008-06-18 08:42:29 +0000

    added new label

--- modules/crpCalendar/pnlang/eng/events.php
+++ modules/crpCalendar/pnlang/eng/events.php
@@ -3,14 +3,14 @@
  * crpCalendar
  *
  * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
- * @author Daniele Conca <jami at cremonapalloza dot org>
- * @license GNU/GPL - v.2
+ * @link http://code.zikula.org/projects/crpcalendar Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
  * @package crpCalendar
  */
 
 //
 define('_CRPCALENDAR_MAXDAY', 'Days interval to show');
 define('_CRPCALENDAR_MAXEVENTS','Maximum number of events');
-
-?>
+// 
+define('_CRPCALENDAR_BLOCK_NO_EVENTS','There are no events in the next');
