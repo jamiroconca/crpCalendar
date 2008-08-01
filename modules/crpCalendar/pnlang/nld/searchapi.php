97c6baee1368bc6719d861b7e1f8b66f1a549cf6
jamiroconca <conca.daniele@gmail.com>
2008-08-01 07:45:11 +0000

    baseline nld for 0.4.5

---
+++ modules/crpCalendar/pnlang/nld/searchapi.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
+ * @version 0.2.0
+ * @author Daniele Conca <jami at cremonapalloza dot org>
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package crpCalendar
+ */
+
+define('_CRPCALENDAR_ARCHIVE', 'Archief');
+define('_CRPCALENDAR_EVENTS', 'Kalender evenementen');
+define('_CRPCALENDAR_SEARCH', 'Kalender evenementen zoeken');
+define('_CRPCALENDAR_SEARCHRESULTS', 'gevonden evenementen');
+define('_CRPCALENDAR_SEACHNONEFOUND', 'Geen evenementen gevonden');
+
+?>
---
+++ modules/crpCalendar/pnlang/nld/user.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007-2008, Daniele Conca
+ * @link http://code.zikula.org/projects/crpcalendar Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
+ * @package crpCalendar
+ */
+
+//
+define('_CRPCALENDAR', 'Kalender');
+
+// user list
+define('_CRPCALENDAR_ADD_PARTECIPATION','Deelnemer toevoegen');
+define('_CRPCALENDAR_ARCHIVED','Gearchiveerd');
+define('_CRPCALENDAR_CONTACT','Contact');
+define('_CRPCALENDAR_CONTENT', 'Evenement detail');
+define('_CRPCALENDAR_CREATEDON','Gecreërd op');
+define('_CRPCALENDAR_DELETE_PARTECIPATION','Deelnemer verwijderen');
+define('_CRPCALENDAR_EVENT', 'Evenement');
+define('_CRPCALENDAR_EVENTS', 'Kalender');
+define('_CRPCALENDAR_EVENTS_MYLIST', 'Ingeschreven voor evenemenent');
+define('_CRPCALENDAR_EVENTS_LIST', 'Kalender evenementen lijst');
+define('_CRPCALENDAR_IMAGES','Images');
+define('_CRPCALENDAR_INVALID_DATE', 'Onjuiste datum');
+define('_CRPCALENDAR_LOCATION','Lokatie');
+define('_CRPCALENDAR_LOCATIONS','vanuit Locations module');
+define('_CRPCALENDAR_NOT_SPECIFIED','Niet gespecificeerd');
+define('_CRPCALENDAR_NOAVATARSELECTED','Geen avatar geselecteerd');
+define('_CRPCALENDAR_ORGANISER','Organiser');
+define('_CRPCALENDAR_STATUS', 'Status');
+define('_CRPCALENDAR_TITLE', 'Titel');
+define('_CRPCALENDAR_UPCOMING','Aankomend');
+define('_CRPCALENDAR_URL','URL');
+define('_CRPCALENDAR_URL_HINT','URL (met http://)');
+
+// event detail
+define('_CRPCALENDAR_DAY_EVENT', 'Dag evenement');
+define('_CRPCALENDAR_END_DATE','Eind datum');
+define('_CRPCALENDAR_EVENT_DOCUMENT','evenement document');
+define('_CRPCALENDAR_EVENT_IMAGE','evenement image (.gif, .jpg, .png) - Max');
+define('_CRPCALENDAR_IMAGE_RESIZE','Image zal (door de browser) geschaald worden naar');
+define('_CRPCALENDAR_NONE', 'Geen');
+define('_CRPCALENDAR_PARTECIPATIONS', 'evenement deelnemers');
+define('_CRPCALENDAR_READS', 'Gelezen');
+define('_CRPCALENDAR_START_DATE','Start datum');
+define('_CRPCALENDAR_SUBMIT', 'Evenement inzenden');
+define('_CRPCALENDAR_VIEW', 'evenementen lijst');
+define('_CRPCALENDAR_USER_PARTECIPATIONS', 'gebruiker ingeschreven voor evenementen');
+define('_CRPCALENDAR_WORDSINTHISTEXT', 'Woorden in tekst');
+
+// form define
+define('_CRPCALENDAR_CURRENT_FILE','Huidig bestand');
+define('_CRPCALENDAR_REQUIRED','*');
+define('_CRPCALENDAR_INVALID_INTERVAL', 'Onjuist datum bereik');
+define('_CRPCALENDAR_WAITING', 'Wacht op goedkeuring.');
+
+//RSS define
+define('_CRPCALENDAR_HCALENDAR','hCalendar microformat');
+define('_CRPCALENDAR_ICAL','crpCalendar iCal evenement');
+define('_CRPCALENDAR_RSS','crpCalendar feed');
+
+// error messages
+define('_CRPCALENDAR_ERROR_DOCUMENT_FILE_SIZE_TOO_BIG','Document grootte niet toegestaan');
+define('_CRPCALENDAR_ERROR_DOCUMENT_NO_FILE','Document bestand niet geladen');
+define('_CRPCALENDAR_ERROR_IMAGE_FILE_SIZE_TOO_BIG','Image bestandsgrootte niet toegestaan');
+define('_CRPCALENDAR_ERROR_IMAGE_NO_FILE','Bestand niet geladen');
+define('_CRPCALENDAR_INVALID_URL','Onjuiste URL');
+
+// days label
+define('_CRPCALENDAR_MON','Ma');
+define('_CRPCALENDAR_TUE','Di');
+define('_CRPCALENDAR_WED','Wo');
+define('_CRPCALENDAR_THU','Do');
+define('_CRPCALENDAR_FRI','Vr');
+define('_CRPCALENDAR_SAT','Za');
+define('_CRPCALENDAR_SUN','Zo');
+
+// view navigation
+define('_CRPCALENDAR_NEXT_DAY','Volgende dag');
+define('_CRPCALENDAR_NEXT_MONTH','Volgende maand');
+define('_CRPCALENDAR_NEXT_YEAR','Volgend jaar');
+define('_CRPCALENDAR_NEXT_WEEK','Volgende week');
+define('_CRPCALENDAR_PREV_DAY','Vorige dag');
+define('_CRPCALENDAR_PREV_MONTH','Vorige maand');
+define('_CRPCALENDAR_PREV_YEAR','Vorige jaar');
+define('_CRPCALENDAR_PREV_WEEK','Vorige week');
+define('_CRPCALENDAR_VIEW_DAY','Dag overzicht');
+define('_CRPCALENDAR_VIEW_MONTH','Maand overzicht');
+define('_CRPCALENDAR_VIEW_WEEK','Week overzicht');
+//
+define('_CRPCALENDAR_VIEW_FORM','Bekijk als');
+define('_CRPCALENDAR_VIEW_LIST','Lijst');
+define('_CRPCALENDAR_VIEW_TABLE','Tabel');
+
+// mail text
+define('_CRPCALENDAR_EVENT_NOTIFICATION','Notificatie van aangemaakt evenement');
+define('_CRPCALENDAR_NOTIFICATION','Evenement aanmaak');
+define('_CRPCALENDAR_DONOTREPLY','Reageert u a.u.b. niet op deze Email! Dit is slechts een automatisch gegenereerde tekst...');
+?>
