59cb8074ffd897e137f51296c928102bb38c02a4
jamiroconca <conca.daniele@gmail.com>
2008-08-01 07:45:56 +0000

    baseline for deu 0.4.0

---
+++ pnlang/deu/admin.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
+ * @version $Id: $
+ * @author Daniele Conca <jami at cremonapalloza dot org>
+ * @license GNU/GPL - v.2
+ * @package crpCalendar
+ *
+ * Translation by sven schomacker aka hilope
+ */
+
+//
+define('_CRPCALENDAR', 'Kalender');
+define('_CRPCALENDAR_GENERAL','Allgemein');
+
+// admin list
+define('_CRPCALENDAR_CONTENT', 'Termindetail');
+define('_CRPCALENDAR_CHANGE_STATUS', 'Status ändern');
+define('_CRPCALENDAR_CHANGE_STATUS_MODIFYING','Termin bearbeiten zum Status ändern');
+define('_CRPCALENDAR_EVENT', 'Termin');
+define('_CRPCALENDAR_EVENTS', 'Termine');
+define('_CRPCALENDAR_NOT_SPECIFIED','nicht gewählt');
+define('_CRPCALENDAR_INVALID_DATE', 'Ungültiges Datum');
+define('_CRPCALENDAR_STATUS', 'Status');
+define('_CRPCALENDAR_TITLE', 'Titel');
+
+// event detail
+define('_CRPCALENDAR_CONTACT','Kontakt');
+define('_CRPCALENDAR_DAY_EVENT', 'Tagestermin');
+define('_CRPCALENDAR_END_DATE','Enddatum');
+define('_CRPCALENDAR_EVENT_IMAGE','Terminbild (.gif, .jpg, .png) - Max');
+define('_CRPCALENDAR_LOCATION','Ort');
+define('_CRPCALENDAR_IMAGE_WIDTH','Terminbildbreite');
+define('_CRPCALENDAR_ORGANISER','Veranstalter');
+define('_CRPCALENDAR_PENDING','wartend');
+define('_CRPCALENDAR_REJECTED','abgelehnt');
+define('_CRPCALENDAR_START_DATE','Startdatum');
+define('_CRPCALENDAR_URL','URL');
+define('_CRPCALENDAR_URL_HINT','URL (mit http://)');
+
+// form define
+define('_CRPCALENDAR_CURRENT_FILE','Aktuelle Datei');
+define('_CRPCALENDAR_DELETE_FILE','Datei löschen');
+define('_CRPCALENDAR_REQUIRED','*');
+define('_CRPCALENDAR_INVALID_INTERVAL', 'Ungültiger Zeitraum');
+define('_CRPCALENDAR_SHOW_FILE','Zeige Datei');
+
+// config
+define('_CRPCALENDAR_FILE_DIMENSION','Maximale Uploadgröße (bytes)');
+define('_CRPCALENDAR_GD_AVAILABLE','GD Library');
+define('_CRPCALENDAR_IMAGE_RESIZE','Bild wird heruntergrechnet auf');
+define('_CRPCALENDAR_IMAGES','Bilder');
+define('_CRPCALENDAR_USE_BROWSER','GD Library fehlgeschlagen');
+define('_CRPCALENDAR_USE_GD','crpCalendar benutzt GD Library');
+define('_CRPCALENDAR_USERLIST_IMAGE','Zeige Vorschauen in Benutzerliste');
+define('_CRPCALENDAR_USERLIST_WIDTH','Benutzerliste Vorschaugröße');
+
+// RSS define
+define('_CRPCALENDAR_ATOM','ATOM');
+define('_CRPCALENDAR_RSS','crpCalendar-Feed');
+define('_CRPCALENDAR_RSS1','RSS 1.0');
+define('_CRPCALENDAR_RSS2','RSS 2.0');
+define('_CRPCALENDAR_ENABLE_RSS','Aktiviere RSS feed');
+define('_CRPCALENDAR_SHOW_RSS','Zeige einen Link zum RSS feed');
+define('_CRPCALENDAR_USE_RSS','Feed-Format');
+
+// error messages
+define('_CRPCALENDAR_ERROR_IMAGE_FILE_SIZE_TOO_BIG','Bildgröße nicht erlaubt');
+define('_CRPCALENDAR_ERROR_IMAGE_NO_FILE','Datei nicht hochgeladen');
+define('_CRPCALENDAR_IMAGE_INVALID_TYPE','Ungültiger Bildtyp');
+define('_CRPCALENDAR_INVALID_URL','Ungültige URL');
+
+?>
---
+++ pnlang/deu/searchapi.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
+ * @version 0.2.0
+ * @author Daniele Conca <jami at cremonapalloza dot org>
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package crpCalendar
+ * 
+ * Translation by sven schomacker aka hilope
+ */
+
+define('_CRPCALENDAR_ARCHIVE', 'Archiv');
+define('_CRPCALENDAR_EVENTS', 'Kalendertermine');
+define('_CRPCALENDAR_SEARCH', 'Kalenderterminsuche');
+define('_CRPCALENDAR_SEARCHRESULTS', 'Termine gefunden');
+define('_CRPCALENDAR_SEACHNONEFOUND', 'Keine Termine gefunden');
+
+?>
