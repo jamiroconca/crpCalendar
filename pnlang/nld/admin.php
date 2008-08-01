4d3764db2f1792f8c0d13ccf65beae179196e940
jamiroconca <conca.daniele@gmail.com>
2008-08-01 07:45:11 +0000

    baseline nld for 0.4.5

---
+++ pnlang/nld/admin.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
+ * @version $Id: $
+ * @author Daniele Conca <jami at cremonapalloza dot org>
+ * @license GNU/GPL - v.2.1
+ * @package crpCalendar
+ */
+
+//
+define('_CRPCALENDAR', 'Kalender');
+define('_CRPCALENDAR_GENERAL','Algemeen');
+
+// admin list
+define('_CRPCALENDAR_CONTENT', 'Evenement detail');
+define('_CRPCALENDAR_CHANGE_STATUS', 'Verander status');
+define('_CRPCALENDAR_CHANGE_STATUS_MODIFYING','Wijzig evenement voor een status verandering');
+define('_CRPCALENDAR_EVENT', 'Evenement');
+define('_CRPCALENDAR_EVENTS', 'Kalender');
+define('_CRPCALENDAR_NOT_SPECIFIED','Niet gespecificeerd');
+define('_CRPCALENDAR_INVALID_DATE', 'Onjuiste datum');
+define('_CRPCALENDAR_STATUS', 'Status');
+define('_CRPCALENDAR_TITLE', 'Titel');
+
+// event detail
+define('_CRPCALENDAR_CLONE_TITLE','Kopie van');
+define('_CRPCALENDAR_CONTACT','Contact');
+define('_CRPCALENDAR_DAY_EVENT', 'Dag evenement');
+define('_CRPCALENDAR_END_DATE','Eind datum');
+define('_CRPCALENDAR_EVENT_DOCUMENT','Evenement document');
+define('_CRPCALENDAR_EVENT_IMAGE','Evenement image (.gif, .jpg, .png) - Max');
+define('_CRPCALENDAR_LOCATION','Lokatie');
+define('_CRPCALENDAR_LOCATIONS','vanuit Locations module');
+define('_CRPCALENDAR_IMAGE_WIDTH','Evenement image breedte');
+define('_CRPCALENDAR_ORGANISER','Organiser');
+define('_CRPCALENDAR_PENDING','Pending');
+define('_CRPCALENDAR_REJECTED','Afgewezen');
+define('_CRPCALENDAR_START_DATE','Start datum');
+define('_CRPCALENDAR_URL','URL');
+define('_CRPCALENDAR_URL_HINT','URL (met http://)');
+
+// form define
+define('_CRPCALENDAR_CURRENT_FILE','Huidig bestand');
+define('_CRPCALENDAR_DELETE_FILE','Verwijder bestand');
+define('_CRPCALENDAR_NONE', 'Geen');
+define('_CRPCALENDAR_REQUIRED','*');
+define('_CRPCALENDAR_INVALID_INTERVAL', 'Onjuist datum bereik');
+define('_CRPCALENDAR_SHOW_FILE','Toon bestand');
+
+// config
+define('_CRPCALENDAR_DOCUMENT_DIMENSION','Max upload document grootte (bytes)');
+define('_CRPCALENDAR_ENABLE_LOCATIONS','Gebruik Locations');
+define('_CRPCALENDAR_ENABLE_PARTECIPATION','Gebruikers kunnen zich inschrijven voor evenement');
+define('_CRPCALENDAR_FILE_DIMENSION','Max upload image grootte (bytes)');
+define('_CRPCALENDAR_GD_AVAILABLE','GD Bibliotheek');
+define('_CRPCALENDAR_IMAGE_RESIZE','Image zal geschaald worden naar');
+define('_CRPCALENDAR_IMAGES','Images');
+define('_CRPCALENDAR_NOTIFICATION_MAIL','Notificatie voor gebruikers bij nieuw (bij leeg geen)');
+define('_CRPCALENDAR_OTHER_MODULES','Andere modules');
+define('_CRPCALENDAR_START_YEAR','Kalender start Jaar');
+define('_CRPCALENDAR_USE_BROWSER','GD Bilibotheek eis niet in orde');
+define('_CRPCALENDAR_USE_GD','crpCalendar gebruik GD Bilibotheek');
+define('_CRPCALENDAR_USERLIST_IMAGE','Toon thumbnails in gebruikers lijst');
+define('_CRPCALENDAR_USERLIST_WIDTH','Gebruiker lijst thumbnail breedte');
+define('_CRPCALENDAR_THEME','crpCalendar theme');
+
+// RSS define
+define('_CRPCALENDAR_ATOM','ATOM');
+define('_CRPCALENDAR_RSS','crpCalendar feed');
+define('_CRPCALENDAR_RSS1','RSS 1.0');
+define('_CRPCALENDAR_RSS2','RSS 2.0');
+define('_CRPCALENDAR_ENABLE_RSS','Aanzetten RSS feed');
+define('_CRPCALENDAR_SHOW_RSS','Toon link naar RSS feed');
+define('_CRPCALENDAR_USE_RSS','Feed formaat');
+
+// error messages
+define('_CRPCALENDAR_ERROR_DOCUMENT_FILE_SIZE_TOO_BIG','Document grootte niet toegestaan');
+define('_CRPCALENDAR_ERROR_DOCUMENT_NO_FILE','Document grootte niet geladen');
+define('_CRPCALENDAR_ERROR_IMAGE_FILE_SIZE_TOO_BIG','Image bestandsgrootte niet toegestaan');
+define('_CRPCALENDAR_ERROR_IMAGE_NO_FILE','Image bestand niet geladen');
+define('_CRPCALENDAR_INVALID_NOTIFICATION','Onjuist notificatie E-Mail adres');
+define('_CRPCALENDAR_IMAGE_INVALID_TYPE','Image onjuist formaat');
+define('_CRPCALENDAR_INVALID_URL','Onjuiste URL');
+
+?>
---
+++ pnlang/nld/contenttypesapi.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
+ * @version $Id: $ 
+ * @author Daniele Conca <jami at cremonapalloza dot org>
+ * @license GNU/GPL - v.2
+ * @package crpCalendar
+ */
+ 
+define('_CRPCALENDAR_CONTENTENTTYPE_EVENTTITLE','Evenement van crpCalendar');
+define('_CRPCALENDAR_CONTENTENTTYPE_EVENTDESCR','Kies een evenement om vanuit crpCalendar weer te geven');
+
+define('_CRPCALENDAR_CONTENTENTTYPE_EVENTLABEL','Selecteer een evenement voor weergave');
+define('_CRPCALENDAR_CONTENTENTTYPE_NOEVENT','Geen evenement');
+
+?>
