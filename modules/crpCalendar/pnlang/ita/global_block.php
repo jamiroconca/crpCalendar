95b1c6aacbf47ca25073f076bf31b686269fe0d1
jamiroconca <conca.daniele@gmail.com>
2008-08-09 10:03:33 +0000

    language update

---
+++ modules/crpCalendar/pnlang/ita/global_block.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://code.zikula.org/projects/crpcalendar Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
+ * @package crpCalendar
+ */
+
+//
+define('_CRPCALENDAR_MAXDAY', 'Intervallo di giorni da visualizzare');
+define('_CRPCALENDAR_MAXEVENTS','Maximum number of events');
+define('_CRPCALENDAR_LOCATION','Luogo');
+define('_CRPCALENDAR_NONE', 'None');
+// 
+define('_CRPCALENDAR_BLOCK_NO_EVENTS','Non ci sono eventi per i prossimi');
+//
+define('_CRPCALENDAR_SUBMIT','Inserisci evento');
+define('_CRPCALENDAR_VIEW','Elenco eventi');
+//
+define('_CRPCALENDAR_RSS','crpCalendar feed');
