256eef77a78fd9e66910e3bda68ea7d68586495d
jamiroconca <conca.daniele@gmail.com>
2008-08-10 15:55:26 +0000

    language update

---
+++ pnlang/ita/monthevents.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * crpCalendar
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://code.zikula.org/projects/crpcalendar Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
+ * @package crpCalendar
+ */
+
+Loader :: includeOnce('modules/crpCalendar/pnlang/ita/global_block.php');
+
+define('_CRPCALENDAR_BLOCK_DISPLAYWEEK','Mostra link alle settimane nella tabella');
+define('_CRPCALENDAR_BLOCK_DISPLAYEVENTS','Mostra link agli eventi nella tabella');
+
+// view navigation
+define('_CRPCALENDAR_NEXT_DAY','Domani');
+define('_CRPCALENDAR_NEXT_WEEK','Settimana successiva');
+define('_CRPCALENDAR_NO_EVENTS','Nessun evento da visualizzare');
+define('_CRPCALENDAR_PREV_DAY','Ieri');
+define('_CRPCALENDAR_PREV_WEEK','Settimana precedente');
+define('_CRPCALENDAR_VIEW_DAY','Vista giornaliera');
+define('_CRPCALENDAR_VIEW_MONTH','Vista mensile');
+define('_CRPCALENDAR_VIEW_YEAR','Vista annuale');
+define('_CRPCALENDAR_VIEW_WEEK','Vista settimanale');
+
+// days label
+define('_CRPCALENDAR_MON','L');
+define('_CRPCALENDAR_TUE','M');
+define('_CRPCALENDAR_WED','M');
+define('_CRPCALENDAR_THU','G');
+define('_CRPCALENDAR_FRI','V');
+define('_CRPCALENDAR_SAT','S');
+define('_CRPCALENDAR_SUN','D');
