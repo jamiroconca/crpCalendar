8d9cc4c05e6aeba19e5de76a8c29e9c4242896f3
jamiroconca <conca.daniele@gmail.com>
2008-08-13 13:13:46 +0000

    language update

--- pnlang/eng/admin.php
+++ pnlang/eng/admin.php
@@ -71,6 +71,7 @@ define('_CRPCALENDAR_USE_BROWSER','GD Library requirement failed');
 define('_CRPCALENDAR_USE_GD','crpCalendar use GD Library');
 define('_CRPCALENDAR_USERLIST_IMAGE','Show thumbnails in user list');
 define('_CRPCALENDAR_USERLIST_WIDTH','User list thumbnail width');
+define('_CRPCALENDAR_SUBMITTED_STATUS','Initial status for events submitted by users');
 define('_CRPCALENDAR_THEME','crpCalendar theme');
 define('_CRPCALENDAR_YEARLIST_CATEGORIZED','Categorized year-list view');
 
