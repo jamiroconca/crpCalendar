307e4e2a79cf34663f64be0c73bcd207d4ec7842
hilope <webmaster@hilope.de>
2008-08-19 11:05:47 +0000

    new german lang files

--- modules/crpCalendar/pnlang/deu/categoryevents.php
+++ modules/crpCalendar/pnlang/deu/categoryevents.php
@@ -3,16 +3,22 @@
  * crpCalendar
  *
  * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
- * @author Daniele Conca <jami at cremonapalloza dot org>
- * @license GNU/GPL - v.2
+ * @link http://code.zikula.org/projects/crpcalendar Support and documentation
+ * @version $Id: $
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
  * @package crpCalendar
- * 
+ *
  * Translation by sven schomacker aka hilope
  */
 
 //
 define('_CRPCALENDAR_MAXDAY', 'Anzeigezeitraum');
 define('_CRPCALENDAR_MAXEVENTS','Maximale Anzahl der Termine');
-
-?>
+//
+define('_CRPCALENDAR_BLOCK_NO_EVENTS','keine weiteren Termine');
+//
+define('_CRPCALENDAR_SUBMIT','Termin melden');
+define('_CRPCALENDAR_VIEW','Terminliste');
+//
+define('_CRPCALENDAR_RSS','crpCalendar-Feed');
--- modules/crpCalendar/pnlang/deu/contenttypesapi.php
+++ modules/crpCalendar/pnlang/deu/contenttypesapi.php
@@ -3,12 +3,12 @@
  * crpCalendar
  *
  * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
+ * @link http://code.zikula.org/projects/crpcalendar Support and documentation
  * @version $Id: $
  * @author Daniele Conca <jami at cremonapalloza dot org>
  * @license GNU/GPL - v.2
  * @package crpCalendar
- * 
+ *
  * Translation by sven schomacker aka hilope
  */
 
