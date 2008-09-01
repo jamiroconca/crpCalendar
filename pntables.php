520d02a6f17def7777b43667474c06038b41f988
jamiroconca <conca.daniele@gmail.com>
2008-09-01 11:07:03 +0000

    module upgrade

--- pntables.php
+++ pntables.php
@@ -11,7 +11,7 @@
 
 /**
  * crpCalendar table information
- * 
+ *
  * @return array table definitions
  */
 function crpCalendar_pntables()
@@ -22,19 +22,20 @@ function crpCalendar_pntables()
 	// Full table definition
 	$pntable['crpcalendar'] = DBUtil :: getLimitedTablename('crpcalendar');
 	$pntable['crpcalendar_column'] = array (
-		'eventid' 		=> 'pn_eventid',
-		'title' 			=> 'pn_title',
-		'urltitle' 		=> 'pn_urltitle',
-		'location' 		=> 'pn_location',
-		'url' 				=> 'pn_url',
-		'contact' 		=> 'pn_contact',
-		'organiser' 	=> 'pn_organiser',
-		'event_text'	=> 'pn_event_text',
-		'start_date'	=> 'pn_start_date',
-		'end_date' 		=> 'pn_end_date',
-		'day_event' 	=> 'pn_day_event',
-		'language' 		=> 'pn_language',
-		'counter' 		=> 'pn_counter'
+		'eventid' 		=> 'eventid',
+		'title' 			=> 'title',
+		'urltitle' 		=> 'urltitle',
+		'location' 		=> 'location',
+		'url' 				=> 'url',
+		'contact' 		=> 'contact',
+		'organiser' 	=> 'organiser',
+		'event_text'	=> 'event_text',
+		'start_date'	=> 'start_date',
+		'end_date' 		=> 'end_date',
+		'day_event' 	=> 'day_event',
+		'language' 		=> 'language',
+		'counter' 		=> 'counter',
+		'id_formicula'=> 'id_formicula',
 	);
 	$pntable['crpcalendar_column_def'] = array (
 		'eventid' 		=> 'I(11) AUTOINCREMENT PRIMARY',
@@ -45,22 +46,24 @@ function crpCalendar_pntables()
 		'contact' 		=> "C(255) NOTNULL DEFAULT ''",
 		'organiser' 	=> "C(255) NOTNULL DEFAULT ''",
 		'event_text' 	=> "X NOTNULL DEFAULT ''",
-		'start_date' 	=> "T DEFAULT NULL",
-		'end_date' 		=> "T DEFAULT NULL",
+		'start_date' 	=> "T NOTNULL DEFAULT '0000-00-00 00:00:00'",
+		'end_date' 		=> "T NOTNULL DEFAULT '0000-00-00 00:00:00'",
 		'day_event' 	=> "I(1) NOTNULL DEFAULT '1'",
 		'language' 		=> "C(30) NOTNULL DEFAULT ''",
-		'counter' 		=> "I(11) NOTNULL DEFAULT '0'"
+		'counter' 		=> "I(11) NOTNULL DEFAULT '0'",
+		'id_formicula'=> "C(255) NOTNULL DEFAULT ''"
 	);
-	
+
 	$pntable['crpcalendar_files'] = DBUtil :: getLimitedTablename('crpcalendar_files');
 	$pntable['crpcalendar_files_column'] = array (
-			'id'						=> 'pn_id',
-			'eventid'				=> 'pn_eventid',
-			'document_type'	=> 'pn_document_type',
-			'name'					=> 'pn_name',
-			'content_type'	=> 'pn_content_type',
-			'size'					=> 'pn_size',
-			'binary_data'		=> 'pn_binary_data');
+			'id'						=> 'id',
+			'eventid'				=> 'eventid',
+			'document_type'	=> 'document_type',
+			'name'					=> 'name',
+			'content_type'	=> 'content_type',
+			'size'					=> 'size',
+			'binary_data'		=> 'binary_data');
+
 	$pntable['crpcalendar_files_column_def'] = array (
 		'id' 						=> 'I(11) AUTOINCREMENT PRIMARY',
 		'eventid' 			=> "I(11) NOTNULL DEFAULT '0'",
@@ -70,22 +73,22 @@ function crpCalendar_pntables()
 		'size' 					=> "I NOTNULL DEFAULT '0'",
 		'binary_data' 	=> "B NOTNULL DEFAULT ''"
 	);
-	
+
 	$pntable['crpcalendar_attendee'] = DBUtil :: getLimitedTablename('crpcalendar_attendee');
 	$pntable['crpcalendar_attendee_column'] = array (
-			'uid'						=> 'pn_uid',
-			'eventid'				=> 'pn_eventid',);
+			'uid'						=> 'uid',
+			'eventid'				=> 'eventid',);
 	$pntable['crpcalendar_attendee_column_def'] = array (
 		'uid' 					=> "I(11) NOTNULL DEFAULT '0'",
 		'eventid' 			=> "I(11) NOTNULL DEFAULT '0'"
 	);
-	
+
 	// Enable categorization services
 	$pntable['crpcalendar_db_extra_enable_categorization'] = pnModGetVar('crpCalendar', 'enablecategorization');
 	$pntable['crpcalendar_primary_key_column'] = 'eventid';
 
 	// add standard data fields
-	ObjectUtil :: addStandardFieldsToTableDefinition($pntable['crpcalendar_column'], 'pn_');
+	ObjectUtil :: addStandardFieldsToTableDefinition($pntable['crpcalendar_column']);
 	ObjectUtil :: addStandardFieldsToTableDataDefinition($pntable['crpcalendar_column_def']);
 
 	return $pntable;
