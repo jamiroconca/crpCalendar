bc0d1528658e053493bc2c4437ea9e03e7908b7e
jamiroconca <conca.daniele@gmail.com>
2009-04-18 17:33:34 +0000

    code reformat

--- modules/crpCalendar/pnlang/eng/contenttypesapi.php
+++ modules/crpCalendar/pnlang/eng/contenttypesapi.php
@@ -1,19 +1,17 @@
 <?php
+
 /**
  * crpCalendar
  *
- * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
- * @version $Id: $ 
- * @author Daniele Conca <jami at cremonapalloza dot org>
- * @license GNU/GPL - v.2
+ * @copyright (c) 2007,2009 Daniele Conca
+ * @link http://code.zikula.org/crpcalendar Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
  * @package crpCalendar
  */
- 
-define('_CRPCALENDAR_CONTENTENTTYPE_EVENTTITLE','Event from crpCalendar');
-define('_CRPCALENDAR_CONTENTENTTYPE_EVENTDESCR','Choose an event to display from crpCalendar');
 
-define('_CRPCALENDAR_CONTENTENTTYPE_EVENTLABEL','Select an event to display');
-define('_CRPCALENDAR_CONTENTENTTYPE_NOEVENT','No event');
+define('_CRPCALENDAR_CONTENTENTTYPE_EVENTTITLE', 'Event from crpCalendar');
+define('_CRPCALENDAR_CONTENTENTTYPE_EVENTDESCR', 'Choose an event to display from crpCalendar');
 
-?>
+define('_CRPCALENDAR_CONTENTENTTYPE_EVENTLABEL', 'Select an event to display');
+define('_CRPCALENDAR_CONTENTENTTYPE_NOEVENT', 'No event');
--- modules/crpCalendar/pnlang/eng/searchapi.php
+++ modules/crpCalendar/pnlang/eng/searchapi.php
@@ -1,12 +1,12 @@
 <?php
+
 /**
  * crpCalendar
  *
- * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
- * @version 0.2.0
- * @author Daniele Conca <jami at cremonapalloza dot org>
- * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @copyright (c) 2007,2009 Daniele Conca
+ * @link http://code.zikula.org/crpcalendar Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
  * @package crpCalendar
  */
 
@@ -15,5 +15,3 @@ define('_CRPCALENDAR_EVENTS', 'Calendar events');
 define('_CRPCALENDAR_SEARCH', 'Calendar events Search');
 define('_CRPCALENDAR_SEARCHRESULTS', 'events found');
 define('_CRPCALENDAR_SEACHNONEFOUND', 'No events found');
-
-?>
