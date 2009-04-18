34a18be023f83348b872c517c83a1857ac5eec6c
jamiroconca <conca.daniele@gmail.com>
2009-04-18 17:34:59 +0000

    version upgrade

--- modules/crpCalendar/pnversion.php
+++ modules/crpCalendar/pnversion.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * crpCalendar
  *
- * @copyright (c) 2007,2008 Daniele Conca
+ * @copyright (c) 2007,2009 Daniele Conca
  * @link http://code.zikula.org/crpcalendar Support and documentation
- * @author Daniele Conca <conca dot daniele at gmail dot com>
+ * @author Daniele Conca <conca.daniele@gmail.com>
  * @license GNU/GPL - v.2.1
  * @package crpCalendar
  */
@@ -12,7 +13,7 @@
 $modversion['name'] = _CRPCALENDAR_NAME;
 $modversion['displayname'] = _CRPCALENDAR_DISPLAYNAME;
 $modversion['description'] = _CRPCALENDAR_DESCRIPTION;
-$modversion['version'] = '0.5.1';
+$modversion['version'] = '0.5.2';
 $modversion['credits'] = 'pndocs/credits.txt';
 $modversion['help'] = 'pndocs/install.txt';
 $modversion['changelog'] = 'pndocs/changelog.txt';
@@ -20,7 +21,7 @@ $modversion['license'] = 'pndocs/license.txt';
 $modversion['official'] = 1;
 $modversion['author'] = 'Daniele Conca - jami';
 $modversion['contact'] = 'conca.daniele@gmail.com';
-$modversion['securityschema'] = array ('crpCalendar::' => 'AuthorID:EventTitle:EventID',
-                                      'crpCalendar::Category' => 'CategoryName::CategoryID');
-
-?>
+$modversion['securityschema'] = array (
+	'crpCalendar::' => 'AuthorID:EventTitle:EventID',
+	'crpCalendar::Category' => 'CategoryName::CategoryID'
+);
