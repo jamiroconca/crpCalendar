a91086c6febdfbd47e98783c4dd9277fb770e30e
jamiroconca <conca.daniele@gmail.com>
2009-09-18 07:49:04 +0000

    language update

--- modules/crpCalendar/pnjavascript/datepicker/lang/it.js
+++ modules/crpCalendar/pnjavascript/datepicker/lang/it.js
@@ -1,13 +1,20 @@
-// italian translation by raf
-
 var fdLocale = {
-        months:[ "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre" ],
-        fullDay:[ "Luned\u00ec", "Marted\u00ec", "Mercoled\u00ec", "Gioved\u00ec", "Venerd\u00ec", "Sabato", "Domenica" ],
-        /*
-        Only stipulate the dayAbbr should the first letter of the fullDay not suffice
-        dayAbbr:[],
-        Only stipulate the firstDayOfWeek should the first day not be Monday
-        firstDayOfWeek:0,
-        */
-        titles:[ "Mese Precedente", "Mese Successivo", "Anno Precedente", "Anno Successivo", "Oggi" ]
+	fullMonths : [ "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio",
+			"Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre",
+			"Dicembre" ],
+	monthAbbrs : [ "Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago",
+			"Set", "Ott", "Nov", "Dic" ],
+	fullDays : [ "Luned\u00ec", "Marted\u00ec", "Mercoled\u00ec",
+			"Gioved\u00ec", "Venerd\u00ec", "Sabato", "Domenica" ],
+	dayAbbrs : [ "Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom" ],
+	titles : [ "Mese precedente", "Mese successivo", "Anno precedente",
+			"Anno successivo", "Oggi", "Apri Calendario", "sett",
+			"Settimana [[%0%]] di [[%1%]]", "Settimana", "Seleziona una data",
+			"Clicca e sposta per mouvere", "Mostra \u201C[[%0%]]\u201D prima",
+			"Vai alla data odierna", "Date disabilitate:" ],
+	firstDayOfWeek : 0
 };
+try {
+	datepickerController.loadLanguage();
+} catch (err) {
+}
