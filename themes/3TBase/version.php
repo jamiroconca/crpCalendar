325066c5b7bdaf66813a7b68d87d21d8ca9b6286
jamiroconca <conca.daniele@gmail.com>
2010-02-28 21:52:20 +0000

    baseline 3TBase theme

---
+++ themes/3TBase/version.php
@@ -0,0 +1,18 @@
+<?php
+$themeversion['name'] = '3TBase';
+$themeversion['display'] = __('3TBase');
+$themeversion['displayname'] = __('3TBase');
+$themeversion['description'] = __("The 3TBase theme is a basic CSS theme.");
+$themeversion['regid'] = '0';
+$themeversion['version'] = '0.1.0';
+$themeversion['official'] = '1';
+$themeversion['author'] = 'Daniele Conca';
+$themeversion['contact'] = 'http://jami.cremonapalloza.org';
+$themeversion['admin'] = 1;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
