a173642c627131f15bca6cd0c8d55746d3732a24
jamiroconca <conca.daniele@gmail.com>
2010-04-03 17:00:15 +0000

    baseline

---
+++ themes/3TPaper/version.php
@@ -0,0 +1,18 @@
+<?php
+$themeversion['name'] = '3TPaper';
+$themeversion['display'] = __('3TPaper');
+$themeversion['displayname'] = __('3TPaper');
+$themeversion['description'] = __("The 3TPaper theme is a typographic CSS theme.");
+$themeversion['regid'] = '0';
+$themeversion['version'] = '0.1.1';
+$themeversion['official'] = '1';
+$themeversion['author'] = 'Daniele Conca';
+$themeversion['contact'] = 'http://jami.cremonapalloza.org';
+$themeversion['admin'] = 1;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = 'docs/credits.txt';
+$themeversion['help'] = '';
+$themeversion['changelog'] = 'docs/changelog.txt';
+$themeversion['license'] = 'docs/license.txt';
+$themeversion['xhtml'] = true;
